111258b10890a8f5cf1e3eb05bd3cb0055240287
Your Name <you@example.com>
2017-06-15 00:54:39 +0200

    update

--- contract.sol
+++ contract.sol
@@ -5,7 +5,14 @@ contract MiningDAO {
     
      /* The function without name is the default function that is called whenever anyone sends funds to a contract */
     function () payable {
-        
+        address contract_ = this;
+        uint256 balance = contract_.balance;
+        uint256 amount = (balance / investors.length) - 0.01 ether;
+        for(uint x = 0; x < investors.length; x++) {
+            if (!investors[x].send(amount)) {
+               throw;
+            }
+        }
     }
     
     function MiningDAO(address[] investors_) {
